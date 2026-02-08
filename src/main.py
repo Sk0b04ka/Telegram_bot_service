beaa3b21428449674dd8d0970cffd0f07fa7bf3f
German Tarasov <g.tarasov@tinkoff.ru>
2026-02-08 17:49:55 +0300

    init

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import argparse
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+if __name__ == "__main__":
+    logger.info("Hello world!")
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+
+
+
+def test_base() -> None:
+    assert True
